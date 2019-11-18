4e98ab47beed0a65202762620e8110f8efb5f086
derblitz00100100 <darkbatchong@gmail.com>
2019-11-18 13:37:41 -0800

    added sorting by name and rank

--- app/src/main/java/com/example/heroes/HeroDetailActivity.java
+++ app/src/main/java/com/example/heroes/HeroDetailActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,13 +16,14 @@ public class HeroDetailActivity extends AppCompatActivity {
     private TextView superpower;
     private TextView ranking;
     private ImageView heroProfile;
+    private Button goBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hero_detail);
         wireWidgets();
-
+        setListeners();
         Intent lastIntent = getIntent();
         Hero heroSelected = lastIntent.getParcelableExtra(HeroesListActivity.EXTRA_HERO);
 
@@ -33,11 +36,23 @@ public class HeroDetailActivity extends AppCompatActivity {
 
     }
 
+    private void setListeners() {
+        goBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent targetIntent = new Intent(HeroDetailActivity.this, HeroesListActivity.class);
+                startActivity(targetIntent);
+                finish();
+            }
+        });
+    }
+
     private void wireWidgets() {
         heroName = findViewById(R.id.textView_heroDetail_name);
         description = findViewById(R.id.textView_heroDetail_description);
         superpower = findViewById(R.id.textView_heroDetail_superpower);
         ranking = findViewById(R.id.textView_heroDetail_ranking);
         heroProfile = findViewById(R.id.imageView_herodetail_profile);
+        goBackButton = findViewById(R.id.button_heroDetail_goback);
     }
 }
